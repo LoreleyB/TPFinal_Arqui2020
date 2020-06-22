f21fa79b87ba93385002d5ee7718729c828febeb
sistop21 <44009546+sistop21@users.noreply.github.com>
2020-06-22 18:32:15 -0300

    variables IF corregidas

--- MIPS_2020/MIPS_2020.srcs/sources_1/new/IFETCH.v
+++ MIPS_2020/MIPS_2020.srcs/sources_1/new/IFETCH.v
@@ -27,56 +27,55 @@ module IFETCH #(
 	//input ctrl_clk_mips,
 	input reset,
 	//input [2:0] i_PC_src,
-	input i_PC_src,
+	input i_pcSrc,
 	//input [len-1:0] in_pc_jump,
-	input [len-1:0] i_PC_branch,
+	input [len-1:0] i_pcBranch,
 	//input [len-1:0] in_pc_register,
 	input stall_flag,
 
-	input [len-1:0] i_address_debug,
-	input debug_flag,
-	input [len-1:0] i_instruc_to_memo,
-	input i_write_enable_ram_inst,
+	input [len-1:0] i_addressDebug,
+	input i_debugFlag,
+	input [len-1:0] i_instructoMemo,
+	input i_writeEnable_inst,
 
-	output reg [len-1:0] o_pc_branch,
+	output reg [len-1:0] o_pcBranch,
 	output [len-1:0] o_instruction,
 	output [len-1:0] o_PC,
-	output reg o_halt_flag_if // para debug
+	output reg o_haltFlag_IF // para debug
     );
 
-    wire [len-1:0] connect_sumador_mux; 
-    wire [len-1:0] connect_mux_pc;
-    wire [len-1:0] connect_pc_sumador_mem;
-    wire [len-1:0] connect_out_instruction;
-    wire [len-1:0] connect_pc_out;
+    wire [len-1:0] w_sumadorMux; 
+    wire [len-1:0] w_muxPc;
+    wire [len-1:0] w_pctoSumadorMem;
+    wire [len-1:0] w_instruction;
     wire w_validI;
-    //wire flush = i_PC_src[0];
-    wire flush = i_PC_src;
+    //wire flush = i_pcSrc[0];
+    wire w_flush = i_pcSrc;
 
-    assign o_instruction = connect_out_instruction;
-    assign o_PC = connect_pc_sumador_mem;
+    assign o_instruction = w_instruction;
+    assign o_PC = w_pctoSumadorMem;
 
 	PC_MUX #(
 		.len(len)
 		)
 		PC_MUX(
 			//.jump(in_pc_jump),
-			.branch(in_pc_branch),
+			.branch(i_pcBranch),
 			//.register(in_pc_register),
-			.i_PC(connect_sumador_mux),
-			.i_select(i_PC_src),
-			.o_PC_MUX(connect_mux_pc)
+			.i_PC(w_pcSumadorMux),
+			.i_select(i_pcSrc),
+			.o_PC_MUX(w_muxPc)
 		); 
 
 	PC #(
 		.len(len)
 		)
 		PC(
-			.i_PC((w_validI)?(connect_pc_sumador_mem):(connect_mux_pc)),
+			.i_PC((w_validI)?(w_pctoSumadorMem):(w_muxPc)),
 			.clk(clk),
 			.reset(reset),
 			.enable(stall_flag),
-			.o_PC(connect_pc_sumador_mem)
+			.o_PC(w_pctoSumadorMem)
 			);
 
 	INSTRUCTION_RAM #(
@@ -88,40 +87,40 @@ module IFETCH #(
 		.RAM_PERFORMANCE("LOW_LATENCY")
 		)
 		INSTRUCTION_RAM(
-			.i_addressI(debug_flag ? i_address_debug : connect_pc_sumador_mem),
+			.i_addressI(i_debugFlag ? i_addressDebug : w_pctoSumadorMem),
 			.clka(clk),
 			.reset(reset),
 			.enable(stall_flag),
-			.i_writeEnable(i_write_enable_ram_inst),
+			.i_writeEnable(i_writeEnable_inst),
 			.o_validI(w_validI),
-			.flush(flush),
-			.o_dataI(connect_out_instruction),
-			.i_dataI(i_instruc_to_memo)
+			.i_flush(w_flush),
+			.o_dataI(w_instruction),
+			.i_dataI(i_instructoMemo)
 			); 
 
 	PC_SUM #(
 		.LEN(len)
 		)
 		PC_SUM(
-			.i_inPC(connect_pc_sumador_mem),
-			.o_outPC(connect_sumador_mux)
+			.i_inPC(w_pctoSumadorMem),
+			.o_outPC(w_pcSumadorMux)
 			); 
 
 
 	always @(posedge clk, posedge reset) 
 	begin
 		if(reset) begin
-			o_pc_branch <= 0;
-			o_halt_flag_if <= 0;			
+			o_pcBranch <= 0;
+			o_haltFlag_IF <= 0;			
 		end
 
         //else if (ctrl_clk_mips) begin
         else begin
-            o_halt_flag_if <= w_validI;
+            o_haltFlag_IF <= w_validI;
     
-            if (stall_flag | flush) 
+            if (stall_flag | w_flush) 
             begin
-                o_pc_branch <= (flush) ? {len{1'b 0}} : connect_sumador_mux;
+                o_pcBranch <= (w_flush) ? {len{1'b 0}} : w_pcSumadorMux;//si es una salto, mando burbuja (nop), sino mando pc + 1
             end
         end
     end
